ba0074758fbcd0449b86ea44ecdd495b5f02ef0c
Maya Mircheva <may4eto@hotmail.com>
2021-01-11 13:24:41 +0100

    changed images uri

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const Highlight = ({color, children}) => (
 const NavItem = ({className, href, children, logo}) => (
     <li className={`mh2-ns f6 f4-l tc ${className}`}>
         <a  className="white no-underline" href={href}>
-            {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
+            {logo ? <img src="./images/logo.svg" className="db center logo" /> : children}
         </a>
     </li>
 )
